a0183deaa70bd80069a3c2f65d1f48701db52310
Doublewealth <doublewealth92@gmail.com>
2023-08-15 16:58:43 -0700

    NO c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def no_c(my_string):
 
-    new_string=""
+def no_c(my_string):
+    new_str = ""
     for i in my_string:
-    if(i !='c')and(i !='C'):
-        return(new_string)
+        if (i != 'c') and (i != 'C'):
+            new_str += i
+            return (new_str)
